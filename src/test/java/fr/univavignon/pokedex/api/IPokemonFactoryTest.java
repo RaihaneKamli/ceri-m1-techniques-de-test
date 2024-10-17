31a5128a09c6ee00480fc9b5b0298a8f595f1882
uapv2500738 <uapv2500738@e-mc131-07.ad.univ-avignon.fr>
2024-10-17 11:27:11 +0200

    IPokemonFactory Initialisation

--- src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
@@ -8,54 +8,31 @@ import org.junit.Test;
 
 public class IPokemonFactoryTest {
 
-    private IPokemonTrainerFactory trainerFactory;  // Mock de la fabrique de dresseur
-    private IPokedexFactory pokedexFactory;  // Mock de la fabrique de pokedex
+	private IPokemonFactory pokemonfact;
 
     @Before
     public void setUp() {
-        // Création des mocks pour IPokemonTrainerFactory et IPokedexFactory
-        trainerFactory = mock(IPokemonTrainerFactory.class);
-        pokedexFactory = mock(IPokedexFactory.class);
+        pokemonfact = mock(IPokemonFactory.class);
     }
 
+    
     @Test
-    public void testCreateTrainerValid() {
-        // Simuler le comportement d'un pokedex pour le trainer
-        IPokedex pokedex = mock(IPokedex.class);
-        when(pokedexFactory.createPokedex(any(), any())).thenReturn(pokedex);
-
-        // Créer un objet PokemonTrainer
-        PokemonTrainer ash = new PokemonTrainer("Ash", Team.VALOR, pokedex);
-
-        // Simuler la création du trainer
-        when(trainerFactory.createTrainer("Ash", Team.VALOR, pokedexFactory)).thenReturn(ash);
-
-        // Vérification du comportement
-        PokemonTrainer trainer = trainerFactory.createTrainer("Ash", Team.VALOR, pokedexFactory);
-        assertNotNull(trainer);
-        assertEquals("Ash", trainer.getName());
-        assertEquals(Team.VALOR, trainer.getTeam());
-        assertEquals(pokedex, trainer.getPokedex());
+    public void createValidPokemon () {
+    	Pokemon Bulbasaur = pokemonfact.createPokemon(0, 613,64, 4000, 4);
+    	Pokemon Aquali = pokemonfact.createPokemon(133, 2729,202, 5000,4);
+    	
+    	if (Bulbasaur.getAttack() < 126 || Bulbasaur.getAttack() > 126+15) {
+            throw new PokedexException("attack need to be between 126 and 141");
+    	}
+    	
+    	if (Aquali.getAttack() < 186 || Aquali.getAttack() > 186+15) {
+            throw new PokedexException("attack need to be between 126 and 141");
+    	}
+    	
+    	
+    	
     }
+    
+    
 
-    @Test
-    public void testCreateTrainerWithNullName() {
-        // Test avec un nom null
-    	PokemonTrainer myTrainer = trainerFactory.createTrainer(null, Team.INSTINCT, pokedexFactory);
-        assertEquals(null,myTrainer);
-    }
-
-    @Test
-    public void testCreateTrainerWithNullTeam() {
-        // Test avec une équipe null
-    	PokemonTrainer myTrainer = trainerFactory.createTrainer("Brock", null, pokedexFactory);
-        assertEquals(null,myTrainer);
-    }
-
-    @Test
-    public void testCreateTrainerWithNullPokedexFactory() {
-        // Test avec un pokedexFactory null
-    	PokemonTrainer myTrainer= trainerFactory.createTrainer("Misty", Team.MYSTIC, null);
-        assertEquals(null,myTrainer);
-    }
 }
