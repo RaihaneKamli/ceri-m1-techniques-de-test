86d8f09c90c6160e8d11c4b12cf48d915b09a123
Raïhane <raihanekamli@gmail.com>
2024-11-15 23:12:02 +0100

    Suite Tests

--- src/main/java/implementation/PokemonFactory.java
+++ src/main/java/implementation/PokemonFactory.java
@@ -8,8 +8,10 @@ public class PokemonFactory implements IPokemonFactory {
     @Override
     public Pokemon createPokemon(int index, int cp, int hp, int dust, int candy) {
         PokemonMetadata metadata = PokemonMetadata.getPokemonMetadataByIndex(index);
-        assert metadata != null;
-        var IV = ((metadata.getAttack() + metadata.getDefense() + metadata.getStamina()) / 45.0) * 100;
-        return new Pokemon(index,metadata.getName(),metadata.getAttack(),metadata.getDefense(),metadata.getStamina(),cp,hp,dust,candy,IV);
+        if (metadata != null) {
+            var IV = ((metadata.getAttack() + metadata.getDefense() + metadata.getStamina()) / 45.0) * 100;
+            return new Pokemon(index,metadata.getName(),metadata.getAttack(),metadata.getDefense(),metadata.getStamina(),cp,hp,dust,candy,IV);
+        }
+        return null;
     }
 }
