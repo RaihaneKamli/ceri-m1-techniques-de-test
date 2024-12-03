62f9bfac6f75ee45d22eca120215db65fd7f2a16
Raïhane <raihanekamli@gmail.com>
2024-12-03 13:42:40 +0100

    Javadoc 1.1

--- src/main/java/fr/univavignon/pokedex/api/IPokedexFactory.java
+++ src/main/java/fr/univavignon/pokedex/api/IPokedexFactory.java
@@ -9,8 +9,8 @@ public interface IPokedexFactory {
 
   /**
    * Creates a new pokedex instance using the given
-   * <tt>metadataProvider</tt> and
-   * <tt>pokemonFactory</tt>.
+   * metadataProvider and
+   * pokemonFactory.
    *
    * @param metadataProvider Metadata provider the created pokedex will use.
    * @param pokemonFactory Pokemon factory the created pokedex will use.
